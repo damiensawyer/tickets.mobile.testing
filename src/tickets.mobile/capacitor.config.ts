bbc4a120377dd3d78eb6f951e43bc3a27f6384ab
Damien Sawyer <damiensawyer@gmail.com>
2021-05-24 22:51:14 +0100

    app id change

--- src/tickets.mobile/capacitor.config.ts
+++ src/tickets.mobile/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'au.org.tickets.mobile',
   appName: 'tickets.mobile',
   webDir: 'build',
   bundledWebRuntime: false
