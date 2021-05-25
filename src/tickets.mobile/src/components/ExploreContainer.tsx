8f45446a5b1f71c66b68b59b77c8bcc05fa96ead
Damien Sawyer <damiensawyer@gmail.com>
2021-05-25 01:29:15 +0100

    image

--- src/tickets.mobile/src/components/ExploreContainer.tsx
+++ src/tickets.mobile/src/components/ExploreContainer.tsx
@@ -1,3 +1,4 @@
+import { IonImg } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
@@ -7,6 +8,9 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
+      
+      <IonImg class="small" src="assets/tickets-logo-colour-rgb.png"/>
+      <h1>Hello Tickets!</h1>
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
