d15a1121dbc0d54f9e11d3f51f4073cb187a4030
Damien Sawyer <damiensawyer@gmail.com>
2021-06-04 17:37:23 +0100

    scopes

--- src/tickets.mobile/src/App.tsx
+++ src/tickets.mobile/src/App.tsx
@@ -1,4 +1,4 @@
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
+import  { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Menu from './components/Menu';
