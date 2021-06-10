3926a154a584b1b4f43741ecdc56a206629d76ae
Damien Sawyer <damiensawyer@gmail.com>
2021-06-10 16:21:56 +0100

    stuff

--- src/tickets.mobile/src/features/Login/LoginPage.tsx
+++ src/tickets.mobile/src/features/Login/LoginPage.tsx
@@ -41,73 +41,59 @@ const selectOptions = {
 
 
 export const LoginPage: React.FC = (b) => {
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [formSubmitted, setFormSubmitted] = useState(false);
     const [usernameError, setUsernameError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
-
-    const login = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setFormSubmitted(true);
-        if (!username) {
-            setUsernameError(true);
-        }
-        if (!password) {
-            setPasswordError(true);
-        }
-
-        if (username && password) {
-            // await setIsLoggedIn(true);
-            // await setUsernameAction(username);
-        }
-    };
-
+    let showError = true
     return (
-        <>
+        <IonCard>
 
             <div className="login-logo">
                 <IonImg class="small" src="assets/tickets-logo-colour-rgb.png"></IonImg>
             </div>
 
-            <form noValidate onSubmit={login}>
-                <IonList>
-                    <IonItem>
-                        <IonLabel position="stacked" color="primary">Username</IonLabel>
-                        <IonInput name="username" type="text" value={username} spellCheck={false} autocapitalize="off" onIonChange={e => setUsername(e.detail.value!)}
-                                  required>
-                        </IonInput>
-                    </IonItem>
 
-                    {formSubmitted && usernameError && <IonText color="danger">
-                        <p className="ion-padding-start">
-                            Username is required
-                        </p>
-                    </IonText>}
+            <IonList>
+                <IonItem>
+                    <IonLabel position="stacked" color="primary">Username</IonLabel>
+                    <IonInput name="username" type="text" value={username} spellCheck={false} autocapitalize="off" onIonChange={e => setUsername(e.detail.value!)}
+                              required>
+                    </IonInput>
+                </IonItem>
 
-                    <IonItem>
-                        <IonLabel position="stacked" color="primary">Password</IonLabel>
-                        <IonInput name="password" type="password" value={password} onIonChange={e => setPassword(e.detail.value!)}>
-                        </IonInput>
-                    </IonItem>
+                {/*need to use useref of something*/}
+                {formSubmitted && true && <IonText color="danger">
+                    <p className="ion-padding-start">
+                        Username is required
+                    </p>
+                </IonText>}
 
-                    {formSubmitted && passwordError && <IonText color="danger">
-                        <p className="ion-padding-start">
-                            Password is required
-                        </p>
-                    </IonText>}
-                </IonList>
+                <IonItem>
+                    <IonLabel position="stacked" color="primary">Password</IonLabel>
+                    <IonInput name="password" type="password" value={password} onIonChange={e => {
+                    }}>
+                    </IonInput>
+                </IonItem>
 
-                <IonRow>
-                    <IonCol>
-                        <IonButton type="submit" expand="block">Login</IonButton>
-                    </IonCol>
-                    <IonCol>
-                        <IonButton routerLink="/signup" color="light" expand="block">Signup</IonButton>
-                    </IonCol>
-                </IonRow>
-            </form>
-        </>
+                {formSubmitted && usernameError && <IonText color="danger">
+                    <p className="ion-padding-start">
+                        Password is required
+                    </p>
+                </IonText>}
+            </IonList>
+
+            <IonRow>
+                <IonCol>
+                    <IonButton type="submit" expand="block" onClick={()=>{setFormSubmitted((true))}}>Login</IonButton>
+                </IonCol>
+                <IonCol>
+                    <IonButton routerLink="/signup" color="light" expand="block">Signup</IonButton>
+                </IonCol>
+            </IonRow>
+        </IonCard>
 
     );
 };
