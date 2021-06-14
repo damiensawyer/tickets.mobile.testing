91cc671d28080d8f96bb9a0dda55dcb082c6d53c
Damien Sawyer <damiensawyer@gmail.com>
2021-06-14 21:21:48 +0100

    wc

---
+++ src/tickets.mobile/src/app/tickets.Rest.ts
@@ -0,0 +1,10 @@
+﻿import * as core from './ticketsCore'
+/*
+What am I trying to achieve?
+- pure functions which make API calls based on an environment object
+- return an option or either
+- 
+
+*/
+let e = core.GetEnvironmentSettings[core.Environment.development]
+e
