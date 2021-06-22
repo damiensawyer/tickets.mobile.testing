f45877ae91ee154d881221c130b092a37cc8b33b
Damien Sawyer <damiensawyer@gmail.com>
2021-06-22 03:21:16 +0100

    will revert

--- src/tickets.mobile/src/App.tsx
+++ src/tickets.mobile/src/App.tsx
@@ -1,4 +1,4 @@
-import {IonApp, IonSplitPane} from '@ionic/react';
+import {IonApp, IonContent, IonList, IonRouterOutlet, IonSplitPane} from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
 import Menu from './components/Menu';
 
@@ -25,18 +25,33 @@ import {useAppSelector} from './app/hooks'
 import * as core from "./app/ticketsCore";
 import React from "react";
 import {Routes} from "./Routes";
+import {Link, Redirect, Route} from "react-router-dom";
+import {HomePage} from "./features/Home/HomePage";
+import {LoginPage} from "./features/Login/LoginPage";
 
 const App: React.FC = () => {
-        core.RunSetup()    
+        core.RunSetup()
         const isLoggedIn = useAppSelector(x => x.loginSlice.isLoggedIn)
         const darkMode = useAppSelector(x => x.settings.darkMode)
         return (
             <IonApp className={darkMode === 'dark' ? 'dark-theme' : ''}>
                 <IonReactRouter>
                     <IonSplitPane contentId="main">
-                        <Routes isLoggedIn={isLoggedIn}/>
-                        <Menu  isLoggedIn={isLoggedIn}/>
-                    </IonSplitPane>;
+                        <IonRouterOutlet id="main">
+                            <Route path="/page/Home" component={HomePage}/>
+                            <Route path="/page/Login" component={LoginPage}/>
+                            <Redirect exact from='/page/Counter' to='/page/Login'/>
+                        </IonRouterOutlet>
+
+                        <IonContent>
+                            <IonList id="menu-list">
+                                <div><Link  to={'/page/Home'}>Home</Link></div>
+                                <div><Link to={'/page/Login'}>Login</Link></div>
+                                <div><Link to={'/page/Counter'}>Counter</Link></div>
+                            </IonList>
+                        </IonContent>
+
+                    </IonSplitPane>
                 </IonReactRouter>
             </IonApp>
         );
