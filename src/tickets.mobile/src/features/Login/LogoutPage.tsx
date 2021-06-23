3652ab23f2ccaa63d6b4950e7246335b5ebe825b
Damien Sawyer <damiensawyer@gmail.com>
2021-06-24 00:41:20 +0100

    remove redirect from logout which was throwing error

--- src/tickets.mobile/src/features/Login/LogoutPage.tsx
+++ src/tickets.mobile/src/features/Login/LogoutPage.tsx
@@ -2,11 +2,15 @@ import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {useHistory, Redirect} from "react-router-dom";
 import {setLoggedOut} from "./LoginSlice";
 import {none, some} from "fp-ts/Option";
+import {useEffect} from "react";
 
 export const LogoutPage: React.FC = (b) => {
     const isLoggedIn = useAppSelector(x => x.loginSlice.isLoggedIn)
     const history = useHistory() // typing from https://stackoverflow.com/questions/49342390/typescript-how-to-add-type-check-for-history-object-in-react
     const dispatch = useAppDispatch();
-    dispatch(setLoggedOut(none))
+    
+    useEffect(()=>{
+        dispatch(setLoggedOut(none))    
+    })
     return null
 };
