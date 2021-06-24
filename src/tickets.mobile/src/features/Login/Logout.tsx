8f3bfe1300fd395ea24f64a8d8e2f2542544633c
Damien Sawyer <damiensawyer@gmail.com>
2021-06-24 02:10:44 +0100

    logout to login

--- src/tickets.mobile/src/features/Login/Logout.tsx
+++ src/tickets.mobile/src/features/Login/Logout.tsx
@@ -4,12 +4,13 @@ import {setLoggedOut} from "./LoginSlice";
 import {none, some} from "fp-ts/Option";
 import {useEffect} from "react";
 import {IonButton} from "@ionic/react";
-
+import {History} from 'history'
 export const Logout: React.FC = (b) => {
     const isLoggedIn = useAppSelector(x => x.loginSlice.isLoggedIn)
     const history = useHistory() // typing from https://stackoverflow.com/questions/49342390/typescript-how-to-add-type-check-for-history-object-in-react
     const dispatch = useAppDispatch();
     return <IonButton onClick={()=>{
         dispatch(setLoggedOut(none))
+        history.push('/page/Login')
     }}>Log out</IonButton>
 };
